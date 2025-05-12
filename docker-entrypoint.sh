421c37a2182fe1e068cd51a762674cebbd3c536b
Sive.Host <66014725+sivehost@users.noreply.github.com>
2025-05-12 11:39:03 +0200

    Update docker-entrypoint.sh

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,21 +12,18 @@ if [[ ! -f /var/www/.import_done ]]; then
     mysql -h "$MYSQL_HOST" -u root -p"$MYSQL_ROOT_PASSWORD" -e "CREATE DATABASE IF NOT EXISTS \`${MYSQL_DATABASE}\`;"
     mysql -h "$MYSQL_HOST" -u root -p"$MYSQL_ROOT_PASSWORD" "$MYSQL_DATABASE" < /tmp/openemrdb.sql
 
-    # Update Apache document root
     echo "Updating Apache DocumentRoot..."
     sed -i 's|DocumentRoot /var/www/html|DocumentRoot /var/www/apps2.frappe.africa|' /etc/apache2/sites-available/000-default.conf
 
-    # Add directory access block
+    echo "Adding Directory access block..."
     cat <<EOF >> /etc/apache2/sites-available/000-default.conf
+<Directory /var/www/apps2.frappe.africa>
+    Options Indexes FollowSymLinks
+    AllowOverride All
+    Require all granted
+</Directory>
+EOF
 
-    <Directory /var/www/apps2.frappe.africa>
-        Options Indexes FollowSymLinks
-        AllowOverride All
-        Require all granted
-    </Directory>
-    EOF
-
-    
     touch /var/www/.import_done
 else
     echo "Import already completed. Skipping."
